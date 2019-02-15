3144e192ed563d31671d117b5f6c8a562abfa98e
benchenchuang <benchenchuang88@sina.com>
2019-02-15 13:01:54 +0800

    build-1

---
+++ build/precache-manifest.01fd51c2d65451e35ce633f84e8e8c18.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = [
+  {
+    "revision": "ca899b00411f4f7bda8129894dc7e2c9",
+    "url": "./static/media/iconfont.ca899b00.eot"
+  },
+  {
+    "revision": "95e621c5d2f717f1ca36768d043ddeff",
+    "url": "./static/media/iconfont.95e621c5.ttf"
+  },
+  {
+    "revision": "94f4e3eb560096a9b74356840f4bd4b8",
+    "url": "./static/media/iconfont.94f4e3eb.woff"
+  },
+  {
+    "revision": "546779c1370ff9e2c553be32aa364921",
+    "url": "./static/media/iconfont.546779c1.svg"
+  },
+  {
+    "revision": "4a686d48d5a089750c49",
+    "url": "./static/js/runtime~main.4a686d48.js"
+  },
+  {
+    "revision": "6f531f186dc5fe73b438",
+    "url": "./static/js/main.6f531f18.chunk.js"
+  },
+  {
+    "revision": "32c2dbc5c5550e536141",
+    "url": "./static/js/1.32c2dbc5.chunk.js"
+  },
+  {
+    "revision": "7f4dc0df65bd688809f52573cf85721a",
+    "url": "./index.html"
+  }
+];
